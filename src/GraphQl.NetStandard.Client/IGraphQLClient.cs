bfe2c18ac00cb75ecc3c931c577554ae4165132e
Chad Oftedahl <chadoftedahl@gmail.com>
2018-02-03 12:26:37 -0600

    Initial code commit

---
+++ src/GraphQl.NetStandard.Client/IGraphQLClient.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+
+namespace GraphQl.NetStandard.Client
+{
+    /// <summary>
+    /// Simple client to provide rudimentary GraphQL querying
+    /// </summary>
+    public interface IGraphQLClient
+    {
+        /// <summary>
+        /// Returns the HTML body of the GraphQL query response as a string
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
+        Task<string> QueryAsync(string query, object variables);
+
+        /// <summary>
+        /// Returns the response deserialized into the provided type.  Only useful for simple responses.
+        /// </summary>
+        /// <param name="query"></param>
+        /// <param name="variables"></param>
+        /// <returns></returns>
+        Task<T> QueryAsync<T>(string query, object variables);
+    }
+}
