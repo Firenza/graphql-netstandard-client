3fc57787950390b82e0a981f2b4f536293ff2d9f
Firenza <chadoftedahl@gmail.com>
2018-03-03 12:10:20 -0600

    initial attempt at adding proper error handling

---
+++ src/GraphQl.NetStandard.Client/GraphQLRequestException.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace GraphQl.NetStandard.Client
+{
+    /// <summary>
+    /// Thrown when a request is not successfully processed
+    /// </summary>
+    public class GraphQLRequestException : Exception
+    {
+        public HttpStatusCode HttpStatusCode { get; set; }
+        public string ResponseBody { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                return $"Request failed with status code of {HttpStatusCode.ToString()}";
+            }
+        }
+
+        private GraphQLRequestException() { }
+
+        public GraphQLRequestException(HttpStatusCode httpStatusCode, string responseBody) : base()
+        {
+            HttpStatusCode = httpStatusCode;
+            ResponseBody = responseBody;
+        }
+    }
+}
---
+++ src/GraphQl.NetStandard.Client/GraphQlQueryException.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GraphQl.NetStandard.Client
+{
+    /// <summary>
+    /// Thrown when a query is processed but has errors
+    /// </summary>
+    public class GraphQLQueryException : Exception
+    {
+        public List<string> ErrorMessages { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                if (ErrorMessages != null)
+                {
+                    return string.Join("|", ErrorMessages);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+        }
+
+        private GraphQLQueryException() { }
+
+        public GraphQLQueryException(IEnumerable<string> errorMessages) : base()
+        {
+            ErrorMessages = errorMessages.ToList();
+        }
+    }
+}
