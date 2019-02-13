a94075487186d623e30b9aaf7d5e00850ce579e8
Firenza <chadoftedahl@gmail.com>
2019-02-13 13:42:43 -0800

    Got edge deserialization working

--- tests/GraphQL.NetStandard.Client.UnitTests/Model/Organization.cs
+++ tests/GraphQL.NetStandard.Client.UnitTests/Model/Organization.cs
@@ -1,7 +1,13 @@
-﻿namespace GraphQL.NetStandard.Client.UnitTests.Model
+﻿using GraphQl.NetStandard.Client;
+using Newtonsoft.Json;
+
+namespace GraphQL.NetStandard.Client.UnitTests.Model
 {
     public class Organization
     {
         public Repository Repository { get; set; }
+
+        [JsonConverter(typeof(GraphQlNodesParentConverter<Team>))]
+        public GraphQlNodesParent<Team> Teams { get; set; }
     }
 }
---
+++ tests/GraphQL.NetStandard.Client.UnitTests/Model/Team.cs
@@ -0,0 +1,21 @@
+﻿using GraphQl.NetStandard.Client;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace GraphQL.NetStandard.Client.UnitTests.Model
+{
+   
+    public class Team
+    {
+        public string Name { get; set; }
+
+        [JsonConverter(typeof(GraphQlNodesParentConverter<Repository>))]
+        public GraphQlNodesParent<Repository> Repositories { get; set; }
+
+        [JsonConverter(typeof(GraphQlEdgesParentConverter<TeamToRepositoryEdge>))]
+        public GraphQLEdgesParent<TeamToRepositoryEdge> RepositoryEdges { get; set; }
+    }
+}
